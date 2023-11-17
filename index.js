743587bfae86038686bc72fe978c79d1741d37dd
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-11-17 15:31:11 +0600

    adding await

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    await client.connect();
 
     // database table name list
     const galleryCollection = client.db("toyKitchen").collection("gallery");
